df786869f283b97b241499c56343ac634f036e0a
morozovbl <morozovbl@iMac-morozovbl.local>
2026-03-07 20:52:23 +0300

    Bug Fix

--- handlers/subscription.py
+++ handlers/subscription.py
@@ -3,6 +3,7 @@ from aiogram.types import CallbackQuery
 
 from database import activate_subscription, get_user
 from .texts import TEXT
+from .start import get_instruction_keyboard
 
 router = Router()
 
@@ -13,5 +14,5 @@ async def subscribe(call: CallbackQuery):
     user = get_user(call.from_user.id)
     lang = user[1]
 
-    await call.message.answer(TEXT["sub_ok"][lang])
+    await call.message.answer(TEXT["sub_ok"][lang], reply_markup=get_instruction_keyboard(lang))
     await call.answer()
